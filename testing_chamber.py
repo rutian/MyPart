6a83534e722113210ae33dc8e6eaddfaea897d7f
Paulosophers <Paulosophers@calvisitor-10-105-139-106.calvisitor.1918.berkeley.edu>
2016-10-13 18:02:39 -0700

    turned back on mypart

--- testing_chamber.py
+++ testing_chamber.py
@@ -42,7 +42,7 @@ def start_counting(hhpc_comport, internal_arduino_comport, raw_sample_folder_pat
 	hhpc.start_count(hhpc_comport) # start the count (non-blocking)
 	print('\t hhpc sampling...')
 	# raw.sample_and_write_analog([0,1,2], 45, raw_sample_folder_path, sample_id ) # raw analog sampling (THIS IS BLOCKING)
-	# arduino.start_mypart_sample(internal_arduino_comport)
+	arduino.start_mypart_sample(internal_arduino_comport)
 	print('\t myparts sampling...')
 
 def record_counts(dylos_comport, csv_path_dylos, hhpc_comport, csv_path_metone, csv_path_ambient, gzll_rfduino_comport, internal_arduino_comport, csv_path_mypart, sample_id):
@@ -53,7 +53,7 @@ def record_counts(dylos_comport, csv_path_dylos, hhpc_comport, csv_path_metone, 
 	print('\t hhpc recorded')
 	record_ambient_readings(sample_id, csv_path_ambient, internal_arduino_comport)
 	print('\t ambient data recorded')
-	# arduino.record_mypart_data(gzll_rfduino_comport, internal_arduino_comport, csv_path_mypart, sample_id)
+	arduino.record_mypart_data(gzll_rfduino_comport, internal_arduino_comport, csv_path_mypart, sample_id)
 	print('\t mypart recorded')
 
 def sleep(sleep_interval, comport):
