211f94af29e0f57caf842a66938fd11ca9bc64a2
Paulosophers <Paulosophers@calvisitor-10-105-136-50.calvisitor.1918.berkeley.edu>
2016-10-19 13:06:56 -0700

    changing data organization in mypart output csv to have all devices in one row

--- arduino_serial_com.py
+++ arduino_serial_com.py
@@ -49,8 +49,12 @@ def start_mypart_sample(ser):
 	time.sleep(3)
 	ser.write(sample_mypart)
 
+
+# output csv: 
+# sample_id, datetime, device_1_small, device_1 large, device_1 humidity, device_1 temp, device_2 ...
 def record_mypart_data(gzll_ser, mypart_ser, num_myparts, csv_path, sample_id):
 	mypart_ser.write(send_mypart_data)
+	device_data = [None] * num_myparts
 	for count in range(0, num_myparts):			
 		d_id = gzll_ser.read(4)
 		device_id = struct.unpack('i', d_id)[0]
@@ -62,6 +66,10 @@ def record_mypart_data(gzll_ser, mypart_ser, num_myparts, csv_path, sample_id):
 		fu3 = struct.unpack('f', f3)[0]
 		f4 = gzll_ser.read(4)
 		fu4 = struct.unpack('f', f4)[0]
-		with open(csv_path, 'a') as csvfile:
-			w = csv.writer(csvfile)
-			w.writerow([sample_id, datetime.datetime.now(), device_id, fu1, fu2, fu3, fu4])
+		device_data[device_id] = [fu1, fu2, fu3, fu4]
+	with open(csv_path, 'a') as csvfile:
+		w = csv.writer(csvfile)
+		row = [sample_id, datetime.datetime.now()]
+		for d in range(0, num_myparts):
+			row.extend(device_data[d])
+		w.writerow(row)
